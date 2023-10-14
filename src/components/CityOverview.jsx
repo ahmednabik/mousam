a56d5bfa494691f73ad172f913d24e57cde9599f
Ahmed Nabi Khan <26043220+ahmednabik@users.noreply.github.com>
2023-10-14 23:47:18 +0500

    City overview UI implemented

--- src/components/CityOverview.jsx
+++ src/components/CityOverview.jsx
@@ -1,3 +1,10 @@
 export default function CityOverview() {
-  return <div className="city-overview">City Overview</div>;
+  return (
+    <div className="city-overview">
+      <h2>Lahore, PK</h2>
+      <p>chances of rain: 0%</p>
+      <h1>31</h1>
+      <img src="./src/assets/sunny.svg" alt="" width="170px" />
+    </div>
+  );
 }
